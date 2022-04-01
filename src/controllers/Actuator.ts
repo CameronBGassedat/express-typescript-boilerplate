cdb9a7b7f36c6e4f7f4a9e55d148fc7ca6bb6977
Maxence Rougé <rouge.maxence31@gmail.com>
2022-04-01 09:28:24 +0200

    add New Endpoints to match CdC

--- src/controllers/Actuator.ts
+++ src/controllers/Actuator.ts
@@ -32,16 +32,6 @@ export default {
     }
   },
 
-  delete : async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      var apiResponse = new ApiResponse("A user has been deleted", []);
-      res.json(apiResponse);
-      return;
-    } catch (error) {
-      next(new ApiResponse("Error", undefined, error as Error));
-    }
-  },
-
   patch : async (req: Request, res: Response, next: NextFunction) => {
     try {
       var apiResponse = new ApiResponse("A user information has been updated", []);
@@ -51,5 +41,14 @@ export default {
       next(new ApiResponse("Error", undefined, error as Error));
     }
   },
-
+  
+  delete : async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      var apiResponse = new ApiResponse("A user has been deleted", []);
+      res.json(apiResponse);
+      return;
+    } catch (error) {
+      next(new ApiResponse("Error", undefined, error as Error));
+    }
+  },
 };
--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -2,15 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import {ApiResponse} from "../Response/Response"
 
 export default {
-  getone: async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      var apiResponse = new ApiResponse("A user has been found", []);
-      res.json(apiResponse);
-      return;
-    } catch (error) {
-      next(new ApiResponse("Error", undefined, error as Error));
-    }
-  },
   getall: async (req: Request, res: Response, next: NextFunction) => {
     try {
       var apiResponse = new ApiResponse("All users has been found", []);
@@ -20,6 +11,17 @@ export default {
       next(new ApiResponse("Error", undefined, error as Error));
     }
   },
+  
+  getone: async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      var apiResponse = new ApiResponse("A user has been found", []);
+      res.json(apiResponse);
+      return;
+    } catch (error) {
+      next(new ApiResponse("Error", undefined, error as Error));
+    }
+  },
+
   post :async (req: Request, res: Response, next: NextFunction) => {
     try {
       var apiResponse = new ApiResponse("A user has been created", []);
@@ -29,6 +31,7 @@ export default {
       next(new ApiResponse("Error", undefined, error as Error));
     }
   },
+
   postfromtoken :async (req: Request, res: Response, next: NextFunction) => {
     try {
       var apiResponse = new ApiResponse("A user has been created from a token", []);
@@ -38,15 +41,7 @@ export default {
       next(new ApiResponse("Error", undefined, error as Error));
     }
   },
-  delete : async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      var apiResponse = new ApiResponse("A user has been deleted", []);
-      res.json(apiResponse);
-      return;
-    } catch (error) {
-      next(new ApiResponse("Error", undefined, error as Error));
-    }
-  },
+
   patch : async (req: Request, res: Response, next: NextFunction) => {
     try {
       var apiResponse = new ApiResponse("A user information has been updated", []);
@@ -56,4 +51,15 @@ export default {
       next(new ApiResponse("Error", undefined, error as Error));
     }
   },
+
+  delete : async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      var apiResponse = new ApiResponse("A user has been deleted", []);
+      res.json(apiResponse);
+      return;
+    } catch (error) {
+      next(new ApiResponse("Error", undefined, error as Error));
+    }
+  },
+
 };

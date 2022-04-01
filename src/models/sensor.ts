c6d85016990c9f24e646a1da253679d7b94a1765
Cameron <cameron.gassedat@ynov.com>
2022-04-01 09:18:35 +0200

    [Fixed] Sensor omits and enum

--- src/models/sensor.ts
+++ src/models/sensor.ts
@@ -1,5 +1,19 @@
 import mongoose from 'mongoose';
 
+enum SensorType{
+    TEMPERATURE = "TEMPERATURE",
+    HUMIDITY = "HUMIDITY",
+    BARO = "BARO",
+    PROXIMITY = "PROXIMITY"
+}
+
+type Sensor = {
+    id: number | string
+    type: SensorType
+    designation : string
+    rawValue: number | boolean
+}
+
 const sensorSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -9,4 +23,9 @@ const sensorSchema = new mongoose.Schema({
     rawValue : Number
 });
 
+type SensorGet = Sensor & {value: string}
+type SensorPost = Omit<Sensor, "id">
+type SensorUpdate = Partial<SensorPost>
+
 module.exports = mongoose.model('Sensor', sensorSchema);
+
